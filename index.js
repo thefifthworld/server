5a2ccdf4a70dd886ee68004fabd86e26fee4ce38
Jason Godesky <jefgodesky@gmail.com>
2020-07-04 15:46:27 -0400

    Add body parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const config = require('./config')
 const pub = require('./routes/public')
@@ -6,6 +7,8 @@ const login = require('./routes/login')
 
 const server = express()
 server.set('view engine', 'ejs')
+server.use(express.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 server.use('/', login)
 server.use('/', pub)
