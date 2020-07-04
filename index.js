112abb3ec781c6201db51861eab8b377936f633f
Jason Godesky <jefgodesky@gmail.com>
2020-07-04 16:37:13 -0400

    Adding session storage to server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
+const passport = require('passport')
 
 const config = require('./config')
 const pub = require('./routes/public')
@@ -9,6 +13,19 @@ const server = express()
 server.set('view engine', 'ejs')
 server.use(express.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cookieParser())
+
+const store = new MySQLStore(config.db)
+server.use(session({
+  key: 'thefifthworld_session',
+  secret: config.sessionSecret,
+  store,
+  resave: true,
+  saveUninitialized: true
+}))
+
+server.use(passport.initialize())
+server.use(passport.session())
 
 server.use('/', login)
 server.use('/', pub)
