6d13c0ec518a5c3eee40d008de2880e666fd3c19
Jason Godesky <jefgodesky@gmail.com>
2020-07-19 19:55:24 -0400

    Display member profile

--- routes/members.js
+++ routes/members.js
@@ -1,12 +1,28 @@
+const axios = require('axios')
 const express = require('express')
 const members = express.Router()
 const { requireLoggedIn } = require('../auth')
+const config = require('../config')
 
 // GET /welcome
 members.get('/welcome', requireLoggedIn, async (req, res) => {
   req.viewOpts.welcome = true
   req.viewOpts.meta.title = 'Set Up Your Profile'
   res.render('member-form', req.viewOpts)
+})
+
+// GET /members/:id
+members.get('/member/:id', async (req, res, next) => {
+  const resp = await axios.get(`${config.api.root}/members/${req.params.id}`)
+  if (resp && resp.status === 200) {
+    req.viewOpts.profile = resp.data
+    if (resp.data && resp.data.links) {
+      const keys = Object.getOwnPropertyNames(resp.data.links)
+      req.viewOpts.profile.showLinks = keys.reduce((acc, curr) => acc && resp.data.links[curr] && resp.data.links[curr].length > 0, true)
+    }
+    return res.render('profile', req.viewOpts)
+  }
+  return next()
 })
 
 module.exports = members
