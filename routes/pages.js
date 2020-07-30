bb3392b511aa1f797e5bb4f4ffb21a05f514c8b6
Jason Godesky <jefgodesky@gmail.com>
2020-07-29 20:19:44 -0400

    Use middleware to load page

--- routes/pages.js
+++ routes/pages.js
@@ -3,17 +3,34 @@ const pages = express.Router()
 const callAPI = require('../api')
 const { checkMessages } = require('../auth')
 
-// GET *
-pages.get('*', checkMessages, async (req, res, next) => {
+/**
+ * Express.js middleware that loads a requested page.
+ * @param req {Object} - The Express.js request object.
+ * @param res {Object} - The Express.js response object.
+ * @param next {function} - The next function to be called.
+ * @returns {Promise} - A Promise that resolves when the page has either been
+ *   loaded from the API or a 404 response has been applied.
+ */
+
+const getPage = async (req, res, next) => {
   try {
-    const resp = await callAPI('GET', `/pages${req.originalUrl}`, req.cookies.jwt)
+    const commands = [ '/edit', '/history', '/compare', '/like', '/unlike' ]
+    const url = commands.includes(req.originalUrl.substr(req.originalUrl.lastIndexOf('/')))
+      ? req.originalUrl.substr(0, req.originalUrl.lastIndexOf('/'))
+      : req.originalUrl
+    const resp = await callAPI('GET', `/pages${url}`, req.cookies.jwt)
     req.viewOpts.meta.title = resp.data.page.title
     req.viewOpts.page = resp.data.page
     req.viewOpts.markup = resp.data.markup
-    res.render('page', req.viewOpts)
-  } catch (err) {
     next()
+  } catch (err) {
+    return res.status(404).render('errors/e404', req.viewOpts)
   }
+}
+
+// GET *
+pages.get('*', getPage, checkMessages, async (req, res, next) => {
+  res.render('page', req.viewOpts)
 })
 
 module.exports = pages
